1706ea0bd1ac3ffc2122dbfaacd6d18cf5305a8b
Pratham <pratham.webdev@gail.com>
2023-01-30 23:17:33 +0530

    added more widgets

--- js/app-dashboard.js
+++ js/app-dashboard.js
@@ -2,7 +2,7 @@
 
 function createCard(el){
 return `
-        <div class="card m-3">
+        <div class="card mx-3 mb-3">
       <div class="card-body">
           <div class="h6 fw-bold text-dark d-flex align-items-center">Test
           </div>
